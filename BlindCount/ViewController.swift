75593b1c3c3d17ba79801653d20d3ae06acdeb55
Arthur <arthur@arthurs-mac.local>
2016-10-28 16:37:53 -0200

    Adicionei cores e animações

--- BlindCount/ViewController.swift
+++ BlindCount/ViewController.swift
@@ -25,30 +25,56 @@ class ViewController: UIViewController {
     var waitingConfirmation = false
     var waitingTimer: Timer!
     
+    let gradient: CAGradientLayer = CAGradientLayer()
+    let neutralColor = UIColor.groupTableViewBackground.cgColor
+    let incrementColor = UIColor(colorLiteralRed: 0.8, green: 0.8, blue: 0.8, alpha: 1).cgColor
+    let decrementColor =  UIColor.white.cgColor
+    let alertColor = UIColor(colorLiteralRed: 0.85, green: 0.8, blue: 0.8, alpha: 1).cgColor
+    let alertColor2 = UIColor(colorLiteralRed: 1, green: 0.8, blue: 0.8, alpha: 1).cgColor
+
+    
     override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
         if (motion == .motionShake) {
             if !waitingConfirmation {
                 AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
                 waitingConfirmation = true
-                waitingTimer = Timer(fire: Date().addingTimeInterval(3), interval: 0, repeats: false) {_ in
-                    print("reset Value")
-                    self.waitingConfirmation = false
+                waitingTimer = Timer(fire: Date().addingTimeInterval(3.375), interval: 0, repeats: false) {_ in
+                    
+                    self.cancelResetTimer()
                     AudioServicesPlaySystemSound(1521)
                 }
                 RunLoop.current.add(waitingTimer, forMode: .defaultRunLoopMode)
+                
+                UIView.animate(withDuration: 0.75, delay: 0.0, options:[.repeat, .allowUserInteraction], animations: {
+                    self.gradient.isHidden = true
+                    self.view.backgroundColor = UIColor(cgColor: self.alertColor)
+                    self.view.backgroundColor = UIColor(cgColor: self.neutralColor)
+                }, completion: { _ in
+                    self.gradient.isHidden = false
+                })
+
             } else {
-                waitingTimer.invalidate()
-                waitingConfirmation = false
+                cancelResetTimer()
                 resetCount()
             }
         }
+    }
+    
+    func cancelResetTimer() {
+        waitingTimer.invalidate()
+        waitingConfirmation = false
+        self.view.layer.removeAllAnimations()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         count = 0
         
-        
+        gradient.frame = view.bounds
+        gradient.colors = [neutralColor, neutralColor, incrementColor, neutralColor, neutralColor]
+       
+        gradient.locations = [0, 1, 1.2, 1.4, 1.6]
+        view.layer.insertSublayer(gradient, at: 0)
         
         //AudioServicesPlaySystemSound(1519) // Peek feedback
         //AudioServicesPlaySystemSound(1520) // Pop feedback
@@ -64,39 +90,95 @@ class ViewController: UIViewController {
     
     func incrementCount() {
         count = count + 1
+        
         AudioServicesPlaySystemSound(1519)
         if waitingConfirmation {
-            waitingTimer.invalidate()
-            waitingConfirmation = false
+            self.cancelResetTimer()
         }
     }
     
-    func decrementCount() {
+    func decrementCount() -> Bool {
+        
+        if waitingConfirmation {
+            self.cancelResetTimer()
+        }
+        
         if count > 0 {
             count = count - 1
             AudioServicesPlaySystemSound(1520)
+            return true
         } else {
             AudioServicesPlaySystemSound(1521)
-        }
-        if waitingConfirmation {
-            waitingTimer.invalidate()
-            waitingConfirmation = false
+            return false
         }
     }
     
-    var vibrateTimer: Timer!
-    var vibrateCount: Int!
+    enum Direction { case Up, Down, Left, Right }
+    
+    func animateGradient(direction: Direction) {
+     
+        CATransaction.begin()
+        CATransaction.setDisableActions(true)
+        switch direction {
+        case .Up, .Down:
+            gradient.startPoint = CGPoint(x: 0.5, y: 0)
+            gradient.endPoint = CGPoint(x: 0.5, y: 1)
+        case .Left, .Right:
+            gradient.startPoint = CGPoint(x: 0, y: 0.5)
+            gradient.endPoint = CGPoint(x: 1, y: 0.5)
+        }
+        CATransaction.commit()
+        
+        let fromValue, toValue: Any
+        switch direction {
+        case .Down, .Right:
+            fromValue = [-0.6, -0.4, -0.2, 0, 1]
+            toValue = [0, 1, 1.2, 1.4, 1.6]
+        case .Up, .Left:
+            fromValue = [0, 1, 1.2, 1.4, 1.6]
+            toValue = [-0.6, -0.4, -0.2, 0, 1]
+        }
+        
+        let animation : CABasicAnimation = CABasicAnimation(keyPath: "locations")
+        animation.fromValue = fromValue
+        animation.toValue = toValue
+        animation.duration = 0.1
+        animation.isRemovedOnCompletion = true
+        animation.fillMode = kCAFillModeForwards
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn)
+        
+        gradient.add(animation, forKey: "increment")
+    }
+    
     func resetCount() {
         
-        vibrateTimer = Timer.scheduledTimer(withTimeInterval: 0.6, repeats: false) {_ in
+        let vibrateTimer = Timer.scheduledTimer(withTimeInterval: 0.6, repeats: false) { _ in
             AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
-            self.count = 0
         }
         RunLoop.current.add(vibrateTimer, forMode: .defaultRunLoopMode)
-        AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
+        
+        self.count = 0
+        
+        AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
+        UIView.animate(withDuration: 1, delay: 0, options:[.allowUserInteraction], animations: {
+            self.gradient.isHidden = true
+            self.view.backgroundColor = UIColor(cgColor: self.alertColor2)
+            self.view.backgroundColor = UIColor(cgColor: self.neutralColor)
+            }, completion: { _ in
+                self.gradient.isHidden = false
+        })
     }
 
     @IBAction func tap(_ sender: UITapGestureRecognizer) {
+        
+        UIView.animate(withDuration: 0.1, delay: 0, options: [.allowUserInteraction], animations: { _ in
+            self.gradient.isHidden = true
+            self.view.backgroundColor = UIColor(cgColor: self.incrementColor)
+            self.view.backgroundColor = UIColor(cgColor: self.neutralColor)
+        }, completion: { _ in
+            self.gradient.isHidden = false
+        })
+        
         incrementCount()
     }
 
@@ -112,11 +194,13 @@ class ViewController: UIViewController {
     }
     
     @IBAction func swiped(_ sender: UISwipeGestureRecognizer) {
-        print(sender.direction)
-        if sender.direction == .left || sender.direction == .down {
+        if sender.direction == .left || sender.direction == .up {
+            animateGradient(direction: sender.direction == .left ? .Left : .Up	)
             incrementCount()
-        } else if sender.direction == .right || sender.direction == .up {
-            decrementCount()
+        } else if sender.direction == .right || sender.direction == .down {
+            if decrementCount() {
+                animateGradient(direction: sender.direction == .right ? .Right : .Down)
+            }
         }
     }
 }
